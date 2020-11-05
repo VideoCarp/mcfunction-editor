1017997e2e7e02642a371ec384b77b9fb1beeb15
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-11-05 19:10:45 +1100

    Update commands.py

--- app/commands.py
+++ app/commands.py
@@ -1,6 +1,7 @@
 # Manages commands.
 from inspect import getsource
-from PyQt5.QtWidgets import QMessageBox
+from webbrowser import open_new_tab as tabbrowser
+from PySide2.QtWidgets import QMessageBox
 def msgbox(info, title="Command Bar"):
     msg = QMessageBox()
     msg.setStyleSheet("""
@@ -45,10 +46,29 @@ def doCommand(inpvar):
     except:
         msgbox("Failed")
 
+def CallCode(code: str):
+    exec(code)
 
 def Execute(var):
     condition = {
-    "help": lambda: msgbox("Help:\n1. `cmd setPath(path)' for non-as saving. Usually automatic.", "Help"),
-    "cmd": lambda: doCommand(var)
+    "help": lambda: msgbox("""
+    1. `help' Displays help menu (this).
+    2. `cmd <function_call>': `eval`s a function call.
+    Available Functions:
+    - `SavePath(location)': Modify the path in which saves are directed.
+    Normally, this is done automatically when you save for the first time.
+    - `CallCode(code: str)': Run some Python code. (can be done with exec too.)
+    3. `wikibedrock': Opens up the 'wiki.bedrock.dev' wiki. (you may omit 'edrock')
+    4. `tutorialbedrock': Opens up 'Commands Tutorial' on bedrock wiki. (you may omit 'edrock')
+    5. `wikijava': Unavailable. I can't find, please create an issue or pull request if you find it. (may omit 'ava')
+    6. `tutorialjava': Opens up 'https://www.digminecraft.com/game_commands/index.php'. 
+    May be a low quality tutorial, if better found issue or pull request. (may omit 'ava')
+    7. `repo': Opens up the mcfunction-editor repository on GitHub.
+    """, "Help"),
+    "cmd": lambda: doCommand(var),
+    "wikib": lambda: tabbrowser("https://wiki.bedrock.dev"),
+    "tutorialb": lambda: tabbrowser("https://wiki.bedrock.dev/tutorials/beginner-commands.html"),
+    "repo": lambda: tabbrowser("https://github.com/VideoCarp/mcfunction-editor/blob/main/app/main.py")
+    "tutorialj": lambda: tabbrowser("https://www.digminecraft.com/game_commands/index.php")
     }
     startsSwitch(var, condition)
