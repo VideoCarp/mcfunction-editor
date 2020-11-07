c491b9f336413471cff3bac370de287942c9e8ba
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-11-07 14:13:44 +1100

    Fix module and saving issue.

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ import sys
 import time
 import highlighting, commands
 from os import path
-from PyQt5.QtGui import QTextDocument, QKeySequence
-from PyQt5.QtWidgets import QApplication,QLabel,QWidget,QGridLayout,QPushButton\
+from PySide2.QtGui import QTextDocument, QKeySequence
+from PySide2.QtWidgets import QApplication,QLabel,QWidget,QGridLayout,QPushButton\
 ,QPlainTextEdit,QFileDialog, QCompleter, QShortcut, QLineEdit
 import threading
 
